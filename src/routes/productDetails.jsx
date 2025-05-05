9248478a2d587b5f458c84333e509d52e8b50141
sanketjadhav31 <sj546400@gmail.com>
2025-05-05 13:09:19 +0530

    first commit

--- src/routes/productDetails.jsx
+++ src/routes/productDetails.jsx
@@ -4,6 +4,7 @@ import { items } from '../models/product.js';
  
 function ProductDetails() {
     const [search, setSearch] = React.useState('');
+    setSearch(items.filter((items) => items.name.toLowerCase().includes(search.toLowerCase())));
     const { id } = useParams();
     const product = items.find((product) => product.id === parseInt(id));
     if (!product) {
@@ -15,14 +16,19 @@ function ProductDetails() {
                 <nav>Home</nav>
                 <nav>About</nav>                
             </nav>
-            
 
             <input type="text" placeholder='Search' className='search' onChange={(e) => setSearch(e.target.value)} />
-            <p>{search}</p>
-            <h2>Product Details</h2>
-            <h1>{product.name}</h1>
-            <p>{product.description}</p>
-            <p>Price: ${product.price}</p>
+         <div>{items.map ((product) => (
+            <div key={product.id} className='product-details'>
+                <h2>{product.name}</h2>
+                <img src={product.image} alt={product.name} />
+                <p>{product.description}</p>
+                <p>Price: ${product.price}</p>
+                <button className='btn'>Add to Cart</button>
+            </div>
+         ))}
+
+         </div>
         </div>
     );
 }
